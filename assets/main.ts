787f81fded47e78337c640cda2bc5511d4b6a2de
muzzik <1226085293@qq.com>
2022-05-05 21:08:11 +0800

    # ...

--- assets/main.ts
+++ assets/main.ts
@@ -1,7 +1,10 @@
 import { _decorator, Component, Node } from "cc";
+import * as cc from "cc";
 const { ccclass, property } = _decorator;
 
 @ccclass("main")
 export class main extends Component {
-  public button_head(): void {}
+	public button_head(): void {
+		cc.resources.loadScene("head");
+	}
 }
